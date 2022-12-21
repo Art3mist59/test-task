04b62e6136a615fcddd333c7c4c4ac7d03873d84
Art3mist9 <Stryapunin9@gmail.com>
2022-12-22 03:29:16 +0500

    Добавлена сортировка данных

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ fetch("http://localhost:3000/tickets")
 
     cheap.onclick = function(){
 
+      cheap.classList.add('buttons__item--checked');
+      fast.classList.remove('buttons__item--checked');
+
+
       if (cheap.classList.contains('buttons__item--checked')) {
         console.log(tickets);
         for (let i = 0; i <= tickets.length - 2; i++) {
@@ -29,12 +33,21 @@ fetch("http://localhost:3000/tickets")
 
     fast.onclick = function(){
 
+      fast.classList.add('buttons__item--checked');
+      cheap.classList.remove('buttons__item--checked');
+
       if (fast.classList.contains('buttons__item--checked')) {
-        for (let i = 0; i <= tickets.length - 1; i++) {
+        for (let i = 0; i <= tickets.length - 2; i++) {
           let minValue = tickets[i];
 
           for (let b = i + 1; b <= tickets.length - 1; b++) {
-            if (tickets[b].segments[0].duration < minValue.segments[0].duration && tickets[b].segments[1].duration < minValue.segments[1].duration && tickets[b].segments[0].duration < minValue.segments[1].duration && tickets[b].segments[1].duration < minValue.segments[0].duration) {
+            if 
+              (tickets[b].segments[0].duration < minValue.segments[0].duration && 
+              tickets[b].segments[1].duration < minValue.segments[1].duration && 
+              tickets[b].segments[0].duration < minValue.segments[1].duration && 
+              tickets[b].segments[0].duration < minValue.segments[0].duration) 
+            {
+
               minValue = tickets[b];
               let swap = tickets[i];
               tickets[i] = minValue;
